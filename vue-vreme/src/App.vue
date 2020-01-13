133502c7b4c09fe4c628cdc0590b08ef8cc6ba80
Bogdan Jude <43954492+kogaion28@users.noreply.github.com>
2020-01-12 19:00:16 -0800

    Update App.vue

--- vue-vreme/src/App.vue
+++ vue-vreme/src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+            <main>
+              <div class="search-box">
+                <input type="text" class="search-bar" placeholder="Cauta..."/>
+              </div>
+            </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return{
+      api_key:'58bc576af3aca496e58eae894a93e9e4'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
+body{
+  font-family: 'montserrat', sans-serif;
+}
+
+
+#app {
+  background-image: url('./assets/rece.jpg');
+  background-size: cover;
+  background-position:bottom;
+  transition: 0.4s; 
+}
+
+main{
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  
+  background-color: rgba(255,255,255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+
+}
+
+.search-box .search-bar:focus{
+box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+background-color: rgba(255,255,255, 0.75);
+border-radius: 16px 0px 16px 0px;
+
+}
+
+
 </style>
